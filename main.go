4cac1b0e83554b8357b705e50a97d5ee8957c0c7
chris erway <cce@appneta.com>
2015-05-08 18:44:55 -0400

    Use local imports in main.go

--- main.go
+++ main.go
@@ -29,9 +29,9 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/tinylib/msgp/gen"
-	"github.com/tinylib/msgp/parse"
-	"github.com/tinylib/msgp/printer"
+	"github.com/appneta/msgp/gen"
+	"github.com/appneta/msgp/parse"
+	"github.com/appneta/msgp/printer"
 	"github.com/ttacon/chalk"
 )
 
